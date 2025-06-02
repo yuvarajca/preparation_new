4aa0e86e44377b025e03d767f5d4e771968d8d79
Abcom <yuvarajca6@gmail.com>
2025-06-02 22:03:16 +0530

    new addition

--- chat.py
+++ chat.py
@@ -1,29 +1,63 @@
-from typing import TypedDict, List
-from langchain_core.messages import HumanMessage
-from langchain_openai import ChatOpenAI
-from langgraph.graph import StateGraph, START, END
+from typing import Annotated, Sequence, TypedDict
 from dotenv import load_dotenv
-import os
+from langchain_core.messages import BaseMessage, ToolMessage, SystemMessage
+from langchain_openai import ChatOpenAI
+from langchain_core.tools import tool
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, END
+from langgraph.prebuilt import ToolNode
 
 load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
 
 class AgentState(TypedDict):
-    messages:List[HumanMessage]
+    messages: Annotated[Sequence[BaseMessage], add_messages]
 
+@tool
+def add(a:int, b:int):
+    """addition function adds two numbers together"""
+    return a+b
 
-llm = ChatOpenAI(model = "gpt-3.5-turbo")
+tools = [add]
+model = ChatOpenAI(model = "gpt-3.5-turbo").bind_tools(tools)
 
-def process(state:AgentState):
-    response = llm.invoke(state['messages'])
-    print(response)
-    return state
+def model_call(state:AgentState):
+    system_prompt = SystemMessage(content = "You're AI system, please answer my query")
+    response = model.invoke([system_prompt]+state['messages'])
+    return {'messages':[response]}
+
+def should_contnue(state:AgentState):
+    messages = state['messages']
+    last_message = messages[-1]
+    if not last_message.tool_calls:
+        return 'end'
+    else:
+        return 'continue'
+    
 
 graph = StateGraph(AgentState)
-graph.add_node("process", process)
-graph.add_edge(START, "process")
-graph.add_edge("process", END)
-agent = graph.compile()
+graph.add_node("our_agent", model_call)
 
-User_input = input("Enter: ")
-agent.invoke({"messages":[HumanMessage(content=User_input)]})
+tool_node = ToolNode(tools = tools)
+graph.add_node("tools", tool_node)
+
+graph.set_entry_point("our_agent")
+graph.add_conditional_edges("our_agent", should_contnue, 
+                            {"continue": "tools",
+                              "end":END
+                              },
+                              )
+
+graph.add_edge("tools", "our_agent")
+app = graph.compile()
+
+def print_stream(stream):
+    for s in stream:
+        message = s['messages'][-1]
+        if isinstance(message, tuple):
+            print(message)
+        else:
+            message.pretty_print()
+
+inputs = {'messages': [("user", "add 3+4")]}
+print_stream(app.stream(inputs, stream_mode = "values"))
+
--- fast_api.py
+++ fast_api.py
@@ -1,12 +1,28 @@
-from fastapi import FastAPI
-api = FastAPI()
+from fastapi import FastAPI, HTTPException
+app = FastAPI()
 
-#GET, POST, PUT, DELETE
-@api.get('/')
-def index():
-    return {'message':'Hello World'}
+BANDS = [
+    {
+        'id':1, 'name':'The Kinks', 'genre': 'Rock'
+    },
+    {
+        'id':2, 'name':'The ship', 'genre': 'Fight'
+    },
+    {
+        'id':3, 'name':'RED', 'genre': 'suspense'
+    },
+    {
+        'id':4, 'name':'Avenger', 'genre':'action'
+    },
+]
 
-@api.get('/getdata')
-def calculation():
-    pass
-    return "calculations done"
+@app.get('/bands')
+async def bands()->list[dict]:
+    return BANDS
+
+@app.get('/bands/{band_id}')
+async def band(band_id:int) -> dict:
+    band = next((b for b in BANDS if b['id'] ==  band_id), None)
+    if band is None:
+        raise HTTPException(status_code = 404, detail='band not found')
+    return band
